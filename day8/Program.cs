5c2718b7afa77d5f90ec0d8bb4be5ce388bc788c
Stef van Hooijdonk <stef.vanhooijdonk@coolblue.nl>
2021-12-10 11:06:44 +0100

    [DAY9] Solutions 1 & 2

---
+++ day8/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AdventOfCode 2021 - day 8.");
+
+            var inputFolder = Environment.CurrentDirectory;
+
+            Console.WriteLine("Reading input.txt from: {0}\r\n",inputFolder);
+            var lines = new List<string>(System.IO.File.ReadAllLines(System.IO.Path.Combine(inputFolder,"input.txt")));
+            Console.WriteLine("Read input.txt with: {0} lines.\r\n",lines.Count);
+
+            var Samples = new List<Sample>();
+
+            foreach(var line in lines){
+                Samples.Add(new Sample(line));
+            }
+
+            var totalEasyDigitsCounted = 0;
+            Samples.ForEach(s=> totalEasyDigitsCounted+= s.CountEasyDigits());
+
+            Console.WriteLine("Step 1: easy digits found {0}.\r\n",totalEasyDigitsCounted);
+
+        }
+    }
+
+    public class Sample{
+        public Sample(string input){
+            var parts = input.Split('|');
+            InputSingals = new List<string>(parts[0].Trim().Split(' '));
+            OutputDigits = new List<string>(parts[1].Trim().Split(' '));
+        }
+
+        public List<string> InputSingals {get;set;}
+        public List<string> OutputDigits {get;set;}
+
+        public int CountEasyDigits(){
+            var result = OutputDigits.FindAll(d => d.Length == 2 ).Count; // digit 1
+            result += OutputDigits.FindAll(d => d.Length == 4 ).Count;// digit 4
+            result += OutputDigits.FindAll(d => d.Length == 3 ).Count;// digit 7
+            result += OutputDigits.FindAll(d => d.Length == 7 ).Count;// digit 8
+            return result;
+        }
+    }
+    public class DigitSegments
+    {
+        public DigitSegments(int digit, string segments){
+            Digit = digit;
+            //Segments = segments.ToCharArray();
+        }
+
+        public int Digit{get;set;}
+        public List<char> Segments {get;set;} = new List<char>();
+    }
+    public class SolvedSample{
+
+        public SolvedSample(Sample sample){
+            Digits.Add(0,"");
+            Digits.Add(1,"");
+            Digits.Add(2,"");
+            Digits.Add(3,"");
+            Digits.Add(4,"");
+            Digits.Add(5,"");
+            Digits.Add(6,"");
+            Digits.Add(7,"");
+            Digits.Add(8,"");
+            Digits.Add(9,"");
+            sampleToSolve = sample;            
+        }
+
+        private Sample sampleToSolve;
+
+        public List<DigitSegments> Digits = new List<DigitSegments>();
+
+        public bool Solve (){
+
+            var edgesToCheck = "abcdefg";
+
+            while(Mapping.Count<10){
+                foreach(var edge in edgesToCheck)
+                {
+                    if(edge == 'a') // 0,2,3,5,6,7,8,9
+                    {
+                        var options = GetSegments(OutputDigits.FindAll(d => d.Length == 3 )); //7
+                        var sureNotOptions = GetSegments(OutputDigits.FindAll(d => d.Length == 1 )); // 1
+
+                        options.RemoveAll(p=> sureNotOptions.Contains(p));
+                        if(options.Length == 1){
+                            Mapping.Add('a', options);
+                        }
+                    }
+
+                    if (edge == 'g') 
+                    {
+                        var options = GetSegments(OutputDigits.FindAll(d => d.Length == 4 || d.Length == 3)); //4&7
+                        var nineOptions = OutputDigits.FindAll(d => d.Length == 6 ); // possible 9
+
+                        var nine = nineOptions.FindAll( d => d.InputSingals.Findall() )
+                        if(options.Length == 1){
+                            Mapping.Add('a', options);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        pri
+
+        private List<char> GetSegments(List<DigitSegments> digits){
+
+            var chars = new List<char>();
+
+            foreach(var d in digits){
+                foreach(var s in d.Segments){
+                    if(!chars.Contains(s)){
+                        chars.Add(s);
+                    }
+                }
+            }
+            return chars;
+        }
+
+        public int Output(){
+
+            return 0;
+        }
+
+        private Dictionary<char,List<char>> Mapping = new Dictionary<char, List<char>>();
+    }
+}
---
+++ day9/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AdventOfCode 2021 - day 9.");
+
+            var inputFolder = Environment.CurrentDirectory;
+
+            Console.WriteLine("Reading input.txt from: {0}\r\n",inputFolder);
+            var lines = new List<string>(System.IO.File.ReadAllLines(System.IO.Path.Combine(inputFolder,"input.txt")));
+            Console.WriteLine("Read input.txt with: {0} lines.\r\n",lines.Count);
+
+            Grid grid = new Grid();
+            
+            for(var row =0; row<lines.Count;row++){
+                grid.AddRow(row, lines[row]);
+            }
+
+            var lowPoints = grid.GetLowPoints();
+
+            var step1 = 0;
+            lowPoints.ForEach(lp => step1 += (1 + lp.CellValue));
+            Console.WriteLine("Found lowpoints: {0} risk {1}.\r\n",lowPoints.Count, step1);
+
+            
+            List<int> basinSizes = new List<int>();
+
+            foreach(var lowpoint in lowPoints){
+                var basin = new List<GridCell>();
+                grid.FindBasin(lowpoint.X, lowpoint.Y, ref basin);
+                basinSizes.Add(basin.Count);
+            }
+            basinSizes.Sort();
+            var step2 = basinSizes[basinSizes.Count-1] * basinSizes[basinSizes.Count-2] * basinSizes[basinSizes.Count-3];
+            Console.WriteLine("Found basins {0}.\r\n", step2);
+        }
+    }
+
+    public class Grid {
+        public Grid(){
+
+        }
+
+        public int MaxY{get;set;}
+        public int MaxX{get;set;}
+
+        public List<GridCell> Cells {get;set;} = new List<GridCell>();
+
+        public void AddRow(int row, string values){
+
+            for(int y=0; y<values.Length;y++){
+                int value = int.Parse(values[y].ToString());
+                Cells.Add(new GridCell(row,y,value));
+                if(y>MaxY) MaxY = y;
+            }
+            MaxX++;
+        }
+
+        public List<GridCell> GetLowPoints(){
+            List<GridCell> lowpoints = new List<GridCell>();
+
+            foreach(var cell in Cells){
+                var neighbours = GetNeighbours(cell);
+
+                if(neighbours.FindAll(n=> n.CellValue > cell.CellValue).Count == neighbours.Count){
+                    lowpoints.Add(cell);
+                }
+            }
+
+            return lowpoints;
+        }
+
+        private List<GridCell> GetNeighbours(GridCell cell){
+            List<GridCell> neighbours = new List<GridCell>();
+
+            if(cell.X > 0){
+                neighbours.AddRange(Cells.FindAll(c=> (c.X == cell.X-1) && (c.Y == cell.Y)));
+            }
+            if(cell.X < MaxX){
+                neighbours.AddRange(Cells.FindAll(c=> (c.X == cell.X+1) && (c.Y == cell.Y)));
+            }
+            if(cell.Y > 0){
+                neighbours.AddRange(Cells.FindAll(c=> (c.Y == cell.Y-1) && (c.X == cell.X)));
+            }
+            if(cell.Y < MaxY){
+                neighbours.AddRange(Cells.FindAll(c=> (c.Y == cell.Y+1) && (c.X == cell.X)));
+            }
+
+            return neighbours;
+        }
+
+        public void FindBasin(int x, int y, ref List<GridCell> basin){
+            var cell = Cells.Find(c=> c.X==x && c.Y==y);
+            if(cell?.CellValue<9 && !basin.Contains(cell)){
+                basin.Add(cell);
+                 
+                if( cell.X > 0 ){
+                    FindBasin(x-1, y, ref basin);
+                }
+                if( cell.X < MaxX ){
+                    FindBasin(x+1, y, ref basin);
+                }
+                if( cell.Y > 0 ){
+                     FindBasin(x, y-1, ref basin);
+                }
+                if( cell.Y < MaxY ){
+                    FindBasin(x, y+1, ref basin);
+                }
+            }
+        }        
+    }
+
+    public class GridCell {
+
+        public GridCell(int locationX, int locationY, int value){
+            X=locationX;
+            Y=locationY;
+            CellValue= value;
+        }
+
+        public int X {get;set;}
+        public int Y {get;set;}
+        public int CellValue {get;set;}
+    }
+}
